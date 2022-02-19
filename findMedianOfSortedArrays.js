9f3ab3974ebb39712538b5dedf2cde9eeb31a792
Salman4Js <92971152+salman4js@users.noreply.github.com>
2022-02-20 02:11:51 +0530

    Add files via upload

---
+++ findMedianOfSortedArrays.js
@@ -0,0 +1,14 @@
+const findMedianSortedArrays = (arr1, arr2) => {
+    const newArr = arr1.concat(arr2);
+    const sortedArr = newArr.sort((a,b) => a -b);
+    console.log(sortedArr);
+    if(sortedArr.length % 2 === 0){
+        const median = sortedArr[sortedArr.length / 2 - 1] + sortedArr[sortedArr.length / 2];
+        return median / 2;
+    } else {
+        const index = Math.floor(sortedArr.length / 2);
+        return sortedArr[index];
+    }
+}
+
+console.log(findMedianSortedArrays([1,2,3], [4,5,6]));
